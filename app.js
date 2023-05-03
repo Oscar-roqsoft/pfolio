f5264677e1e390011128feb910528069f69682e3
Nnoje Oscar <oscar-tech@Nnojes-MacBook-Pro.local>
2023-05-02 18:01:13 -0700

    myPortfolio

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ navLink.forEach(function(link){
 
 // scroll section
 const section = document.querySelectorAll("section")
-const navLinks = document.querySelectorAll(".nav-link")
+const navLinks = document.querySelectorAll("header nav a")
 
 window.onscroll = function(){
     section.forEach(function(item){
@@ -40,18 +40,53 @@ window.onscroll = function(){
         let id = item.getAttribute("id")
         if( top >= offset && top < offset + height){
             navLinks.forEach(function(link){
+                const headlink = document.querySelector(`header nav a[href='#${id}' ]`)
                link.classList.remove("active")
-               document.querySelector(`header nav a[href='#${id}' ]`).classList.add("active")
+               headlink.classList.add("active")
             })
         }
     })
 
-// ============================  sticky top(how to add sticky top)  ===================================
-
-let header = document.querySelector("header")
-header.classList.toggle("sticky",window.scrollY)
 
 }
+
+
+// ============================  portfolio btn js  ===================================
+const portfolioBtn = document.querySelectorAll(".portfolio-btn")
+const sectionCon = document.querySelector(".services-container")
+const sectionText = document.querySelectorAll(".p")
+
+sectionCon.addEventListener("click", function(e){
+    e.preventDefault()
+    let id = e.target.id
+    if (id){ // it means if the id is true
+            sectionText.forEach(function(list){
+                if(e.target.previousElementSibling.classList.contains("hide")){
+                    list.classList.remove("hide")
+                    list.nextElementSibling.classList.add("hide")
+                    e.target.previousElementSibling.previousElementSibling.classList.add("hide")
+                    e.target.previousElementSibling.classList.remove("hide")
+                }else{
+                    list.classList.remove("hide")
+                    list.nextElementSibling.classList.add("hide")
+                    e.target.previousElementSibling.previousElementSibling.classList.remove("hide")
+                    e.target.previousElementSibling.classList.add("hide")
+                }
+            })
+            portfolioBtn.forEach((btn)=>{
+                if(e.target.textContent === "Read More"){
+                    btn.textContent = "Read More"
+                    console.log(btn)
+                    e.target.textContent = "End"
+                }else{
+                    btn.textContent = "Read More"
+                    console.log(btn)
+                    e.target.textContent = "Read More"
+                }
+            })
+       
+    }
+})
 
 
 // ============================  scroll reveal  ===================================
@@ -67,6 +102,7 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content ', { origin: "bottom" });
 ScrollReveal().reveal('.heading ', { origin: "top" });
 ScrollReveal().reveal('h3 ', { origin: "left" });
+ScrollReveal().reveal('.form-inner ', { origin: "bottom" });
 
 
 // ============================  for typed js  ===================================
@@ -78,3 +114,26 @@ const typed = new Typed(".mtext", {
      loop: true
 })
 
+
+
+// ============================  form ===================================
+ function sendEmail(){
+    Email.send({
+        SecureToken: "46e06c13-a0f7-41b2-9f2b-6ff5265ab159 ",
+        To : 'oscar.nnoje@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "This is the subject",
+        Body : document.getElementById("message").value + `<br>` + document.getElementById("email").value
+    }).then(
+        message => alert(message)
+        )
+ }
+
+
+
+
+
+
+
+
+//  C7C7759A69C415CB9AE090C6D576C83F162Dqw
